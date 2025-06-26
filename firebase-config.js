8d8053083ce7964afd808fa5ff60052565b620d4
asgshopfree <mahiur.xxv@gmail.com>
2025-06-26 15:23:02 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDuDQ5UrRuwads8BAbZEFOaN4Od0URRvoU",
+  authDomain: "mistxmahi.firebaseapp.com",
+  databaseURL: "https://mistxmahi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mistxmahi",
+  storageBucket: "mistxmahi.appspot.com",
+  messagingSenderId: "815225536989",
+  appId: "1:815225536989:web:bf191212500a8626f23634",
+  measurementId: "G-90EETTD3ZS"
+};
+firebase.initializeApp(firebaseConfig);
