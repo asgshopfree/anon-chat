e98523ccfe21756e45481851eddb827966e069ad
asgshopfree <mahiur.xxv@gmail.com>
2025-06-27 16:38:13 +0600

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,24 @@
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+
+// Initialize Firebase (replace with your config)
+firebase.initializeApp({
+  apiKey: "AIzaSyDuDQ5UrRuwads8BAbZEFOaN4Od0URRvoU",
+  authDomain: "mistxmahi.firebaseapp.com",
+  databaseURL: "https://mistxmahi-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "mistxmahi",
+  messagingSenderId: "881105906685",
+  appId: "1:881105906685:web:fc45fc4f9797f4e6f9c70c"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Background message received', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: '/icon.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
