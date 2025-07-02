141c71c43549df1bc8192ea2c9680d2c4d312617
asgshopfree <mahiur.xxv@gmail.com>
2025-07-02 17:31:41 +0600

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,10 +5,10 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-comp
 firebase.initializeApp({
   apiKey: "AIzaSyDuDQ5UrRuwads8BAbZEFOaN4Od0URRvoU",
   authDomain: "mistxmahi.firebaseapp.com",
-  databaseURL: "https://mistxmahi-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "mistxmahi",
   messagingSenderId: "881105906685",
-  appId: "1:881105906685:web:fc45fc4f9797f4e6f9c70c"
+  appId: "1:881105906685:web:fc45fc4f9797f4e6f9c70c",
+  databaseURL: "https://mistxmahi-default-rtdb.asia-southeast1.firebasedatabase.app"
 });
 
 const messaging = firebase.messaging();
