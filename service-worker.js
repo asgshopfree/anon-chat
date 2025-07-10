d1510d22f6fc15e3c93ec1010c7c62abde26e04a
asgshopfree <mahiur.xxv@gmail.com>
2025-07-10 13:34:11 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "chatroom-v5"; // bump version to bust old cache
+const CACHE_NAME = "chatroom-v4"; // 🔁 bump version when you change HTML/CSS/JS
 
 const FILES_TO_PRECACHE = [
   "/",
@@ -14,18 +14,18 @@ const FILES_TO_PRECACHE = [
 ];
 
 self.addEventListener("install", (event) => {
-  console.log("SW installing...");
-  self.skipWaiting();
+  self.skipWaiting(); // install immediately
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_PRECACHE))
   );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("SW activating...");
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      )
     )
   );
   self.clients.claim();
@@ -35,18 +35,19 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     fetch(event.request)
       .then(response => {
-        return caches.open(CACHE_NAME).then(cache => {
-          cache.put(event.request, response.clone());
-          return response;
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
         });
+        return response;
       })
       .catch(() => caches.match(event.request))
   );
 });
 
-// Allow skipWaiting via postMessage
+// Allow skipWaiting on message
 self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
+  if (event.data === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
